b7b62fba3de0a2ea765e01a186e2fe4e355d3be5
Muhammad Rahman <muhammmad.rahman@aemedia.com>
2010-11-21 20:48:08 +0000

    settings changes

--- settings_qa.py
+++ settings_qa.py
@@ -13,7 +13,7 @@ MANAGERS = ADMINS
 DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
 DATABASE_NAME = 'sewinzco_iflatshare'             # Or path to database file if using sqlite3.
 DATABASE_USER = 'sewinzco'             # Not used with sqlite3.
-DATABASE_PASSWORD = 'Sh)h@g!5!'         # Not used with sqlite3.
+DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = 'localhost'             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
