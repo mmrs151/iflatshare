86687a9ed883940ecb306374a958f19039b6daba
Muhammad Rahman <muhammmad.rahman@aemedia.com>
2011-03-10 23:57:53 +0000

    [10985783] Ordered by date and filter by user

--- core/admin.py
+++ core/admin.py
@@ -3,7 +3,9 @@ from django.contrib import admin
 
 class ItemAdmin(admin.ModelAdmin):
     date_hierarchy = 'purchase_date'
-    list_display = ('name','price','user','purchase_date','category', 'user_address')    
+    list_display = ('name','price','user','purchase_date','category', 'user_address')  
+    list_filter = ('user',)
+    ordering = ('-purchase_date',)
 
 admin.site.register(Address)
 admin.site.register(Item, ItemAdmin)
