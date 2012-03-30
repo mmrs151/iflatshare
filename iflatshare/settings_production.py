b5291652c11b7f3cff46dd8e8e4785e0db07ae82
Muhammad.rahman <muhammad.rahman@tangentlabs.co.uk>
2012-03-30 11:29:22 +0100

    Auto logout for qa and production

--- iflatshare/settings_production.py
+++ iflatshare/settings_production.py
@@ -98,6 +98,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'core.middleware.AutoLogout',
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
